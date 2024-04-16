942439f7fdd2c25d663b98a9350b183c3cca45bd
sambbaahh <samikukkonen00@hotmail.com>
2024-04-16 18:40:16 +0300

    testing..

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from "@playwright/test";
+
 export default defineConfig({
-  testDir: "tests",
   webServer: [
     {
       command: "npm run start-frontend",
-      url: "http://127.0.0.1:5173",
+      url: "http://localhost:5173",
       timeout: 120 * 1000,
     },
     {
       command: "npm run start-backend",
-      url: "http://127.0.0.1:5000",
+      url: "http://localhost:5000",
       timeout: 120 * 1000,
     },
   ],
-  use: { baseURL: "http://127.0.0.1:5173" },
+  use: { baseURL: "http://localhost:5173" },
 });
