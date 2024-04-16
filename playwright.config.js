c0de5705b957aa70fd607bf2e1b0e29dc3002978
sambbaahh <samikukkonen00@hotmail.com>
2024-04-16 18:23:35 +0300

    tests now work?

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,17 @@
 import { defineConfig } from "@playwright/test";
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "tests",
+  webServer: [
+    {
+      command: "npm run start-frontend",
+      url: "http://127.0.0.1:5173",
+      timeout: 15 * 1000,
+    },
+    {
+      command: "npm run start-backend",
+      url: "http://127.0.0.1:5000",
+      timeout: 15 * 1000,
+    },
+  ],
+  use: { baseURL: "http://127.0.0.1:5173" },
 });
