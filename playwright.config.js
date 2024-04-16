5d7833c5e1c31d3e54fa8d80dfa54cb3da6f03e5
sambbaahh <samikukkonen00@hotmail.com>
2024-04-16 18:43:55 +0300

    testdir

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "tests",
+  testDir: "./tests",
   webServer: [
     {
       command: "npm run start-frontend",
